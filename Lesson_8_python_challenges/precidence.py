f2c18c92119e8164417f890b35c2803d758964cd
utishtr-jpg <utishtr@gmail.com>
2026-02-08 20:20:55 -0800

    precidence

---
+++ Lesson_8_python_challenges/precidence.py
@@ -0,0 +1,18 @@
+v=4
+w=8
+x=9
+y=5
+z=7
+
+z = (v+w) * x / y
+print("Value of (v+w) * x / y is,",x)
+
+
+name = "Luke"
+age = 16
+
+if name == "Luke" or name == "Kevin" and age <= 5:
+    print("Welcome!")
+else:
+    print("Goodbye!")
+    #end
---
+++ guest.py
@@ -0,0 +1,10 @@
+sentence="Utisht is the chillest guy who ever lived"
+
+print("chillest" in sentence)
+print("Sussy" in sentence)
+print("Python" not in sentence)
+color=["Red","Blue","Green", "Purple"]
+print(type (color))
+print("Red" in color)
+print("Orange" in color)
+print("Yellow" not in color)

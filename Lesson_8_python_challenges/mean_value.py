66193b1f0ab8bc2466e479f5184af447267eb221
utishtr-jpg <utishtr@gmail.com>
2026-02-08 20:43:49 -0800

    Add files via upload

---
+++ Lesson_8_python_challenges/mean_value.py
@@ -0,0 +1,14 @@
+mean1= 38
+wrong=36
+right= 56
+total=40
+
+sum=mean1 * total
+print("The sum of 40 number:",sum)
+
+num2=sum-(wrong)-(right)
+print("sum-((wrong))-(right)", num2)
+
+mean2=num2/total
+print(mean2)
+#end
